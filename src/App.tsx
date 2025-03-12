5b4a239aa530d9ed6a864f1efd0b20b2451f8029
Kevin Liu <kevin.liu4@mail.mcgill.ca>
2025-03-12 18:26:37 -0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,9 @@
-<<<<<<< HEAD
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1 className="text-4xl text-center text-blue-500">Hello from Vite</h1>
-=======
-import './App.css'
-
-function App() {
-  return (
-    <>
-      	<h1 className='text-4xl text-center text-blue-500'>
-          	Hello from Vite
-      	</h1>
->>>>>>> 1c3522009e08bcf9f6d7fcbe176741e484db539a
     </>
   );
 }
--- src/components/QRCodeDisplay.tsx
+++ src/components/QRCodeDisplay.tsx
@@ -1,20 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import { fetchQRCode } from '../api/QRCodeApi';
+import React, { useEffect, useState } from "react";
+import { fetchQRCode } from "../api/QRCodeApi";
 
 const QRCodeDisplay: React.FC = () => {
-    const [qrCodeUrl, setQrCodeUrl] = useState<string>('');
+  const [qrCodeUrl, setQrCodeUrl] = useState<string>("");
 
-    useEffect(() => {
-        fetchQRCode()
-            .then((url) => setQrCodeUrl(url))
-            .catch((error) => console.error('Error loading QR code:', error));
-    }, []);
+  useEffect(() => {
+    fetchQRCode()
+      .then((url) => setQrCodeUrl(url))
+      .catch((error) => console.error("Error loading QR code:", error));
+  }, []);
 
-    return (
-        <div>
-            {qrCodeUrl && <img src={qrCodeUrl} alt="QR Code" />}
-        </div>
-    );
+  return <div>{qrCodeUrl && <img src={qrCodeUrl} alt="QR Code" />}</div>;
 };
 
 export default QRCodeDisplay;
