46a377ae2e0e62e9a97b2d28e59a7c0acae4003e
David Zhou <david.zhou.0110@gmail.com>
2025-03-12 10:33:32 -0400

    FIX: Removed testing code in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css'
-import QRCodeDisplay from './components/QRCodeDisplay'
 
 function App() {
   return (
     <>
       	<h1 className='text-4xl text-center text-blue-500'>
           	Hello from Vite
-          	<QRCodeDisplay />
       	</h1>
     </>
   )
