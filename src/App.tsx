4c107f40903d66e34e52056e98dcb31c15297cf6
coreyzhao <coreyzh2004@gmail.com>
2025-03-13 14:04:00 -0400

    test: dev-env to main - cd test

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ function App() {
     <>
       <h1 className="text-4xl text-center text-blue-500">Hello from Vite</h1>
       <h1 className="text-4xl text-center text-blue-500">Hello from Vite</h1>
-      <h1 className="text-4xl text-center text-blue-500">Hello from Vite</h1>
     </>
   );
 }
