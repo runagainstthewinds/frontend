6de04424d0e136746dbc5cb4b9d40747121e4fcb
coreyzhao <coreyzh2004@gmail.com>
2025-03-13 09:24:58 -0400

    test: terraform / CD test

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <>
       <h1 className="text-4xl text-center text-blue-500">Hello from Vite</h1>
+      <h1 className="text-4xl text-center text-blue-500">Hello from Vite</h1>
     </>
   );
 }
