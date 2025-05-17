9c277fdff7f6da4d2317e2cc62165e0c9430aeb1
David Zhou <david.zhou.0110@gmail.com>
2025-05-16 22:32:42 -0400

    MTN: Changed condition

--- src/components/historypage/recent-runs-card.tsx
+++ src/components/historypage/recent-runs-card.tsx
@@ -39,9 +39,7 @@ export default function RecentRunsCard() {
   const recentRuns = useMemo(() => {
     return [...pastRuns]
       .filter(run => 
-        run.achievedPace !== null && 
-        run.achievedDistance !== null && 
-        run.achievedDuration !== null
+        run.isComplete
       )
       .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
       .slice(0, ITEMS_PER_PAGE);
